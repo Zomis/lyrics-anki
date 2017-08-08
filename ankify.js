d88cd229bbb1f759aba5ea839267820e0707aa5e
Simon Forsberg <zomis2k@hotmail.com>
2017-08-08 14:26:08 +0200

    Add function for ankify

--- ankify.js
+++ ankify.js
@@ -1,3 +1,18 @@
+function ankify(lyrics, translation) {
+  if (typeof translation === "undefined" || translation === null) {
+    translation = "";
+  }
+  lyrics = lyrics.split("\n").map(line => line.trim());
+  translation = translation.split("\n").map(line => translation.trim());
+  if (translation.length > 0 && translation.length != lyrics.length) {
+    throw "Translation must have the same amount of lines as lyrics";
+  }
+
+  let result = ["Result"];
+
+  return result.join("\n");
+}
+
 const fs = require("fs");
 
 if (process.argv.length < 3) {
@@ -7,10 +22,10 @@ if (process.argv.length < 3) {
 
 const filename = process.argv[2];
 
-const data = fs.readFileSync(filename, "utf8");
+const lyrics = fs.readFileSync(filename, "utf8");
 console.log("Using lyrics file: " + filename);
 
-const translation = null;
+let translation = null;
 
 if (process.argv.length > 3) {
   const filenameTranslation = process.argv[3];
@@ -18,7 +33,11 @@ if (process.argv.length > 3) {
   translation = fs.readFileSync(filenameTranslation, "utf8");
 }
 
-fs.writeFile(filename + ".anki", "Result", err => {
+const result = ankify(lyrics, translation);
+console.log();
+console.log("Result:");
+console.log(result);
+fs.writeFile(filename + ".anki", result, err => {
   if (err) {
     throw err;
   }

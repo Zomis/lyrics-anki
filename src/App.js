53faa2aaec1f4e78560d7aba106476c906b17f93
Simon Forsberg <zomis2k@hotmail.com>
2017-08-08 13:30:25 +0200

    Submit and change third text area

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Flexbox from "flexbox-react";
 
 class App extends Component {
   constructor(props) {
@@ -9,16 +10,21 @@ class App extends Component {
       lyrics: "Freude, schöner Götterfunken\nTochter aus Elysium,",
       translation: "Joy, beautiful spark of divinity,\nDaughter from Elysium,"
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   processLyrics = (lyrics, translation) => {};
 
   updateState = (name, event) => {
-    console.log("Update " + name + " to " + event);
     let state = this.state;
     state[name] = event.target.value;
     this.setState(state);
   };
+
+  handleSubmit(event) {
+    this.updateState("result", { target: { value: "Hello World" } });
+    event.preventDefault();
+  }
 
   render() {
     return (
@@ -31,17 +37,30 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <form onSubmit={this.handleSubmit}>
+          <Flexbox flexDirection="row" minWidth="100vh">
+            <Flexbox flexGrow={1}>
+              <textarea
+                cols={60}
+                rows={30}
+                className="lyrics"
+                value={this.state.lyrics}
+                onChange={event => this.updateState("lyrics", event)}
+              />
+            </Flexbox>
+            <Flexbox flexGrow={1}>
+              <textarea
+                cols={60}
+                rows={30}
+                className="translation"
+                value={this.state.translation}
+                onChange={event => this.updateState("translation", event)}
+              />
+            </Flexbox>
+          </Flexbox>
+          <input type="submit" value="Submit" />
           <textarea
-            className="lyrics"
-            value={this.state.lyrics}
-            onChange={event => this.updateState("lyrics", event)}
-          />
-          <textarea
-            className="translation"
-            value={this.state.translation}
-            onChange={event => this.updateState("translation", event)}
-          />
-          <textarea
+            cols={60}
+            rows={30}
             className="result"
             value={this.state.result}
             onChange={event => this.updateState("result", event)}
